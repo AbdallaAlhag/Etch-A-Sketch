9e603bc5bda09e1625b72363e817b510886ca6f2
Abdalla Alhag <alhagabdalla@gmail.com>
2023-06-13 14:34:32 -0700

    created canvas and grid with borders

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const grid = document.querySelector('.grid');
+    let width = 16;
+
+
+    function createCanvas() {
+        for (let i = 0; i < width*width; i++) {
+            const square = document.createElement('div');
+            square.setAttribute('id', i);
+            grid.appendChild(square);
+        }
+    }
+
+    createCanvas();
+
+})
