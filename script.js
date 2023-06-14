7e312be7166ba78cdd2375c658e9d9f31bd6df47
Abdalla Alhag <alhagabdalla@gmail.com>
2023-06-14 14:14:38 -0700

    added the slider and the js code with it

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     const grid = document.querySelector('.grid');
     let width = 16;
+    const btnClear = document.querySelector('#clear');
+    btnClear.addEventListener('click', () => {
+        resetBoard();
+    });
 
+    const slider = document.getElementById("myRange");
+    let output = document.getElementById("size");
+    output.textContent = slider.value + "x" + slider.value;
+    
+
+    slider.oninput = function() {
+        output.textContent = this.value + "x" + this.value;
+        //createCanvas();
+        let newWidth = slider.value;
+        //recreateCanvas();
+    }
+
+    
 
     function createCanvas() {
         for (let i = 0; i < width*width; i++) {
@@ -9,21 +26,30 @@ document.addEventListener("DOMContentLoaded", () => {
             square.setAttribute('id', i);
             grid.appendChild(square);
 
-
             // hover over
-            square.addEventListener('mouseover', function(e){
-                square.style.backgroundColor = "black";
-            });
-
-            // square.addEventListener('mouseout', function(e){
-            //     square.style.backgroundColor = "white";
+            hoverOver(square);
+            // square.addEventListener('mouseover', function(e){
+            //     square.style.backgroundColor = "black";
             // });
-
         }
     }
 
-
-
+   
     createCanvas();
+    
+    function hoverOver(block) {
+        block.addEventListener('mouseover', function(e){
+            square.style.backgroundColor = "black";
+        });
+    }
+
+    function resetBoard(){
+        for (let i = 0; i < width*width; i++){
+            const element = document.getElementById(i);
+            element.style.backgroundColor = "white";
+        }
+        
+    }
 
 })
+
